4c816ec4ee3893fd2eef66e2793679f61ffeda37
larry <lmoiola@gmail.com>
2019-11-29 17:03:05 -0800

    adding models for membership and membership type

--- clc_reg/admin.py
+++ clc_reg/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from .models import VerifyRegistration
+from .models import Membership
+from .models import MembershipType
 
 admin.site.register(VerifyRegistration)
+admin.site.register(Membership)
+admin.site.register(MembershipType)
--- clc_reg/models.py
+++ clc_reg/models.py
@@ -6,6 +6,23 @@ def is_confirmed(user):
     return confirmed.confirmed
 User.add_to_class('is_confirmed', is_confirmed)
 
+def membership_isactive(user):
+    isactive = user.MembershipLevel.get()
+    return isactive.is_active
+User.add_to_class('membership_isactive', membership_isactive)
+
+def membership_level(user):
+    level = user.MembershipLevel.get()
+    return level.type
+User.add_to_class('membership_level', membership_level)
+
+class MembershipType(models.Model):
+    name       = models.CharField(max_length=200)
+    type       = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
 class VerifyRegistration(models.Model):
     confirmation_code = models.CharField(max_length=200)
     expiration        = models.DateTimeField()
@@ -14,3 +31,12 @@ class VerifyRegistration(models.Model):
 
     def __str__(self):
         return self.user.username + ': ' + self.confirmation_code
+
+class Membership(models.Model):
+    membership = models.ForeignKey(MembershipType, on_delete=models.PROTECT)
+    is_active   = models.BooleanField()
+    expiration = models.DateTimeField()
+    user       = models.ForeignKey(User, on_delete=models.PROTECT, related_name='MembershipLevel')
+
+    def __str__(self):
+        return self.user.username + ': ' + self.membership.name
