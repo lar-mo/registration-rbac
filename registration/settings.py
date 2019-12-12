10af74b88bbc510959e47be60f195ab29e6372e4
larry <lmoiola@gmail.com>
2019-12-11 18:21:35 -0800

    more adjustments to images on index, upsell, plus, premium

--- registration/settings.py
+++ registration/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'clc_reg',
-        'USER': 'clc_user',
-        'PASSWORD': '1RXuS5X?',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'clc_reg',
+#         'USER': 'clc_user',
+#         'PASSWORD': '1RXuS5X?',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
